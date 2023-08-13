731147b748cea00f278fc784bc1478ed58d2d459
AnveeMehrotra <87015902+AnveeMehrotra@users.noreply.github.com>
2023-08-13 20:35:32 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+video=createCapture(VIDEO);
+video.size(600,500);
+
+canvas=createCanvas(400,300);
+canvas.postion(500,100)
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
+  
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!');
+}
+
+
+function gotposes(){
+    
+}
